40be8b9a6797cb93b3302b957fb9471a9a8b1cca
Habib Ghaffari Hadigheh <ghaffh1@mcmaster.ca>
2022-01-19 19:59:57 -0500

    Changing the main file to support raw featue downloader

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from data_loader import AccumulatedDataLoader
-import featureExtraction.tappingHelpers as tappingHelper
-import featureExtraction.balanceHelper as balanceHelper
+
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
@@ -28,4 +27,4 @@ def data_loader():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # data_loader()
-    balanceHelper.tested_jason()
+    data_loader()
