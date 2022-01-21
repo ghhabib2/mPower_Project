7df87d1b973b6a27c85df8a65ff64cb3a99accdc
Habib Ghaffari Hadigheh <ghaffh1@mcmaster.ca>
2022-01-21 02:20:47 -0500

    Make changes to the main to plot the signals

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from data_loader import AccumulatedDataLoader
+from featureExtraction import balanceHelper,gaitHelpers, tappingHelpers, memoryHelpers
+from utils import signal_plot
+import os
+import numpy as np
 
+ROOT_PATH = os.path.join(os.getcwd(),"collected_data")
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
@@ -23,8 +28,36 @@ def data_loader():
     # TODO Do outlier detection.
     # TODO Try different clustering algorithms.
 
+def plotter():
+    balance_file_path = os.path.join(ROOT_PATH,"data_moition_signal_sample.csv")
+    balance_file_distance_path = os.path.join(ROOT_PATH, "data_moition_signal_sample_distance.csv")
+    tapinter_file_path = os.path.join(ROOT_PATH, "tap_inter.csv")
+
+    balance_axis_file = np.genfromtxt(balance_file_path, delimiter=",")
+    balance_distance_file = np.genfromtxt(balance_file_distance_path, delimiter=",")
+    tapinter_file = np.genfromtxt(tapinter_file_path, delimiter=",")
+
+    balance_time = balance_axis_file[:, 0]
+    balance_x = balance_axis_file[:, 1]
+    balance_y = balance_axis_file[:, 2]
+    balance_z = balance_axis_file[:, 3]
+    balance_distance = balance_distance_file
+
+    signal_plot.signal_plotter(balance_time, balance_x, "Balance signal X axis")
+    signal_plot.signal_plotter(balance_time, balance_y, "Balance signal Y axis")
+    signal_plot.signal_plotter(balance_time, balance_z, "Balance signal Z axis")
+    signal_plot.signal_plotter(balance_time, balance_distance, "Balance Distance from origin signal")
+    signal_plot.array_plot(tapinter_file, "Tapping Interval")
+
+
+
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # data_loader()
-    data_loader()
+    # balanceHelper.tested_jason()
+    #  tappingHelpers.tested_jason()
+    memoryHelpers.tested_jason()
+    #plotter()
+
